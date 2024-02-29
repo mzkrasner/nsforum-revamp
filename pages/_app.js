e370f41d05d2a9b788268ab2872eda66fa49e8b0
reneedaos <43126993+reneedaos@users.noreply.github.com>
2024-02-28 22:13:54 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,13 +10,13 @@ import { GlobalContext } from "../contexts/GlobalContext";
  * Set the global forum context here (you can create categories using the dashboard by clicking on "Create a sub-context"
  * from your main forum context)
  */
-global.orbis_context = "kjzl6cwe1jw148542am3d42ytqg4ngzgnystjkzz7m9img4e4exv2gzg5n90rjj";
+global.orbis_context = "kjzl6cwe1jw148u8qk0m6b8tukb7rw7as9123dbkeutx3mc3kl96hf0g7e81opi";
 
 /**
  * Set the global chat context here (the chat displayed when users click on the "Community Chat" button).
  * The Community Chat button will be displayed only if this variable is set
  */
-global.orbis_chat_context = "kjzl6cwe1jw148542am3d42ytqg4ngzgnystjkzz7m9img4e4exv2gzg5n90rjj";
+global.orbis_chat_context = "kjzl6cwe1jw148u8qk0m6b8tukb7rw7as9123dbkeutx3mc3kl96hf0g7e81opi";
 
 let orbis = new Orbis({
   useLit: true,
