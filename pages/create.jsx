039925ea37c426041ed2a17707f8742cf3b0dd46
Michael Jola-Moses <jolamoses@gmail.com>
2024-02-29 03:46:01 +0100

    Removed create an attestaion button

--- pages/create.jsx
+++ pages/create.jsx
@@ -1,19 +1,19 @@
-import React, { use, useEffect, useState } from "react";
-import Head from "next/head";
-import Editor from "../components/Editor";
-import AttestEditor from "../components/AttestEdit";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Sidebar from "../components/Sidebar";
-import Footer from "../components/Footer";
-import { useOrbis, User } from "@orbisclub/components";
+import React, { use, useEffect, useState } from 'react';
+import Head from 'next/head';
+import Editor from '../components/Editor';
+import AttestEditor from '../components/AttestEdit';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
+import { useOrbis, User } from '@orbisclub/components';
 
 export default function Create() {
   const { orbis, user, setConnectModalVis } = useOrbis();
   const [create, setCreate] = useState(false);
 
   useEffect(() => {
-    if(global.orbis_context){
+    if (global.orbis_context) {
       console.log(global.orbis_context);
     }
     if (user) {
@@ -24,53 +24,52 @@ export default function Create() {
     <>
       <Head>
         {/** Title */}
-        <title key="title">Share a new post | Network Society Forum</title>
+        <title key='title'>Share a new post | Network Society Forum</title>
         <meta
-          property="og:title"
-          content="Share a new post | Network Society Forum"
-          key="og_title"
+          property='og:title'
+          content='Share a new post | Network Society Forum'
+          key='og_title'
         />
 
         {/** Description */}
         <meta
-          name="description"
-          content="Discuss the future of Network Societies"
-          key="description"
+          name='description'
+          content='Discuss the future of Network Societies'
+          key='description'
         ></meta>
         <meta
-          property="og:description"
-          content="Discuss the future of Network Societies"
-          key="og_description"
+          property='og:description'
+          content='Discuss the future of Network Societies'
+          key='og_description'
         />
-        <link rel="icon" href="/favicon.png" />
+        <link rel='icon' href='/favicon.png' />
       </Head>
-      <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip bg-main">
-        <div className="antialiased">
-          <div className="min-h-screen flex">
+      <div className='flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip bg-main'>
+        <div className='antialiased'>
+          <div className='min-h-screen flex'>
             {/*  Page content */}
-            <main className="grow overflow-hidden">
+            <main className='grow overflow-hidden'>
               {/*  Site header */}
               <Header />
               <Hero
-                title="Sharing a new post on the Network Society Forum"
-                description="You are about to share a new post. Make sure to read our rules before doing so."
+                title='Sharing a new post on the Network Society Forum'
+                description='You are about to share a new post. Make sure to read our rules before doing so.'
                 image
               />
 
               {/* Page content */}
               <section>
-                <div className="max-w-6xl mx-auto px-4 sm:px-6">
-                  <div className="md:flex md:justify-between">
+                <div className='max-w-6xl mx-auto px-4 sm:px-6'>
+                  <div className='md:flex md:justify-between'>
                     {/* Show post editor or connect button */}
-                    <div className="md:grow pt-0 pb-12 pr-10">
+                    <div className='md:grow pt-0 pb-12 pr-10'>
                       {user ? (
                         <>
-                          
                           {create ? (
                             <>
-                            <AttestEditor context={global.orbis_context}/>
-                            <button
-                                className="btn-sm py-1.5 btn-secondary mt-3"
+                              <AttestEditor context={global.orbis_context} />
+                              <button
+                                className='btn-sm py-1.5 btn-secondary mt-3'
                                 onClick={() => setCreate(false)}
                               >
                                 Back
@@ -78,25 +77,25 @@ export default function Create() {
                             </>
                           ) : (
                             <>
-                            <Editor />
-                            <div className="w-full text-center bg-slate-50 rounded border border-primary bg-stone-300	 p-6">
+                              <Editor />
+                              {/* <div className="w-full text-center bg-slate-50 rounded border border-primary bg-stone-300	 p-6">
                               <button
                                 className="btn-sm py-1.5 btn-secondary"
                                 onClick={() => setCreate(true)}
                               >
                                 Create an Attestation
                               </button>
-                            </div>
+                            </div> */}
                             </>
                           )}
                         </>
                       ) : (
-                        <div className="w-full text-center bg-slate-50 rounded border border-primary bg-secondary p-6">
-                          <p className="text-base text-secondary mb-2">
+                        <div className='w-full text-center bg-slate-50 rounded border border-primary bg-secondary p-6'>
+                          <p className='text-base text-secondary mb-2'>
                             You must be connected to share a post in this forum.
                           </p>
                           <button
-                            className="btn-sm py-1.5 btn-main"
+                            className='btn-sm py-1.5 btn-main'
                             onClick={() => setConnectModalVis(true)}
                           >
                             Connect
