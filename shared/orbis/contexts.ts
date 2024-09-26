f74818943b35756b4f5b7c943205168aaf0a671c
Michael Jola-Moses (JMM) <jolamoses@gmail.com>
2024-09-26 15:41:34 +0100

    Added default context

--- shared/orbis/contexts.ts
+++ shared/orbis/contexts.ts
@@ -1,7 +1,5 @@
 const contexts = {
-  content: "kjzl6kcym7w8y8btg67hqcqbqs0u3hudz2tc1ktw3ybkej4zq7xzwfbvxbgull0",
-  reactions: "kjzl6kcym7w8yajt689s13c59kkmnvs4cygr9wz8xu28i062apurlxx9a70bsug",
-  root: "kjzl6kcym7w8yafuyhsg26usod2vjbnop4v0t1djhxol6fyn8spwc9xnmwh4k3l",
+  root: "kjzl6kcym7w8y90om619g15j3kl7xvi1wviapr2ng4jn28h3nkrf97jncfsay8a",
 } as const;
 
 export default contexts;
