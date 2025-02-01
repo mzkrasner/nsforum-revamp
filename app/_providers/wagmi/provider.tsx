d08163ac1d7e1268b55f4dee61c9818e918cc04d
Mark Krasner <mzkrasner@gmail.com>
2025-02-01 12:02:06 -0500

    Removing unused module

--- app/_providers/wagmi/provider.tsx
+++
@@ -1,9 +0,0 @@
-import { WagmiProvider } from "@privy-io/wagmi";
-import { PropsWithChildren } from "react";
-import config from "./config";
-
-const AppWagmiProvider = ({ children }: PropsWithChildren) => {
-  return <WagmiProvider config={config}>{children}</WagmiProvider>;
-};
-
-export default AppWagmiProvider;
